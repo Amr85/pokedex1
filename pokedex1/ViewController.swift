52e7ba0607b5efd298d7d629b8e4e67ece414865
Amr Isreb <amrisreb@Amrs-Mac-2.local>
2017-08-07 13:29:34 -0700

    Initial Commit

---
+++ pokedex1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex1
+//
+//  Created by Amr Isreb on 8/7/17.
+//  Copyright © 2017 Truga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
